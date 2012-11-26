3884cece281fcd1c4b6973e51c7e0cbb54c3406c
Folke Palmqvist <folke.palmqvist@webbits.nu>
2012-11-26 19:14:34 +0100

    Ändrat utseendet på innehållet

--- Hemsida/assets/pages/home.php
+++ Hemsida/assets/pages/home.php
@@ -1,3 +1,25 @@
-<div class="hero-unit">
-	<p>Grundmall</p>
+<div class="row">
+	<div class="span8">
+		<div class="hero-unit">
+			<center><h1>BILD HÄR</h1></center>
+		</div>
+	</div>
+	<div class="span4">
+		<div class="hero-unit">
+			<table class="table table-bordered">
+				<tr>
+					<td>Medlemmar</td>
+					<td>123</td>
+				</tr>
+				<tr>
+					<td>Trådar</td>
+					<td>123</td>
+				</tr>
+				<tr>
+					<td>Inlägg</td>
+					<td>123</td>
+				</tr>
+			</table>
+		</div>
+	</div>
 </div>
